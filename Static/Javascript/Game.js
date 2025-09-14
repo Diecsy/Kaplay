3b51b119a9d57a30d330cdb255c4dc8c8f0b16a5
lmao <116317098+Diecsy@users.noreply.github.com>
2025-09-13 21:50:46 -0700

    Basic Socket.IO rules, etc

--- Static/Javascript/Game.js
+++ Static/Javascript/Game.js
@@ -1,5 +1,16 @@
 import { ClientService } from './Services/Client.js';
 import { DebugService } from './Services/Debug.js';
+import { io } from "socket.io-client";
+
+let ClientId = localStorage.getItem("ClientId");
+if (!ClientId) {
+  ClientId = crypto.randomUUID();
+  localStorage.setItem("ClientId", ClientId);
+}
+
+const Socket = io("35.160.120.126:3000", {
+  Auth: { ClientId }
+});
 
 kaplay({
     crisp: true,
@@ -7,6 +18,12 @@ kaplay({
     canvas: document.getElementById("KaplayCanvas"),
     letterBox: true,
     maxFPS: 60,
+});
+
+Socket.on("ConnectionError", (Error) => {
+  if (Error.message === "SingleTab") {
+    alert("You already have this app open in another tab.");
+  }
 });
 
 DebugService.ErudaConsole();
--- index.js
+++ index.js
@@ -8,13 +8,24 @@ var IO = require("socket.io")(HTTPs);
 var StaticPath = Path.join(__dirname, "Static");
 Application.use(Express.static(StaticPath));
 
-const ServerState = {
-    Clients: {},
-};
+const ActiveClients = new Set();
+const ServerState = {};
+
 
 IO.on("connection", (Socket) => {
+    const ClientId = Socket.handshake.auth.clientId;
+
+    if (!ClientId) {
+        Socket.disconnect(true);
+        return;
+    }
+
+    ActiveClients.add(ClientId);
+    console.log(`Client ${ClientId} connected`);
+
     Socket.on("disconnect", () => {
-        delete ServerState.Clients[Socket.id];
+        ActiveClients.delete(ClientId);
+        console.log(`Client ${ClientId} disconnected`);
     });
 });
 
